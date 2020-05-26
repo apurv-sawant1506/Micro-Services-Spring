78a43b4a508baab1c038fc9a9365bbbbdab46fb9
Apurv Sawant <apurv.sawant17@vit.edu>
2020-05-27 01:30:11 +0530

    Added Spring Cloud Config Server and Local Git Repo

---
+++ spring-cloud-config-server/src/main/java/com/in28minutes/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.in28minutes.microservices.springcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+//We need the @EnableConfigServer annotation to enable the spring cloud config server
+@EnableConfigServer
+@SpringBootApplication
+public class SpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+	}
+
+}
+//We can fetch the details of limits-service by the location: localhost:8888/limits-service/default
